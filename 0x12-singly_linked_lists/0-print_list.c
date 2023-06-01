8bfa5d5cfd849d9e92bb62d6d4e55dd523ee29f7
“yasrai” <etudesyasmine@gmail.com>
2023-06-01 05:11:16 -0700

    0x12-singly_linked_lists

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -2,13 +2,14 @@
 #include "lists.h"
 
 /**
- * print_list - prints all the elements of a linked list
+ *  prints all the elements of a linked list
  * @h: pointer to the list_t list to print
  * Return: the number of nodes printed
  */
 size_t print_list(const list_t *h)
 {
 	size_t s = 0;
+
 	while (h)
 	{
 		if (!h->str)
@@ -18,5 +19,6 @@ size_t print_list(const list_t *h)
 		h = h->next;
 		s++;
 	}
+
 	return (s);
 }
--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -5,9 +5,12 @@
  * free_list - frees a linked list
  * @head: list_t list to be freed
  */
+
 void free_list(list_t *head)
+
 {
 	list_t *temp;
+
 	while (head)
 	{
 		temp = head->next;
