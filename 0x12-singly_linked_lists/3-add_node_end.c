e4889f9724f72e8da33d8271e7f4e1913a0f70fb
“yasrai” <etudesyasmine@gmail.com>
2023-06-01 03:31:17 -0700

    0x12-singly_linked_lists

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -2,32 +2,29 @@
 #include <string.h>
 #include "lists.h"
 /**
- * add_node_end - adds a new node at the end of a linked list
- * @head: double pointer to the list_t list
- * @str: string to put in the new node
- *
- * Return: address of the new element, or NULL if it failed
- */
-list_t *add_node_end(list_t **head, const char *str)
+  * add_nodeint_end - add new nodes to the end of the list
+  * @head: current place in the list
+  * @n: int to add to the list
+  * Return: pointer to current position in list
+  */
+listint_t *add_nodeint_end(listint_t **head, const int n)
 {
- list_t *new;
- list_t *temp = *head;
- unsigned int len = 0;
- while (str[len])
- len++;
- new = malloc(sizeof(list_t));
- if (!new)
- return (NULL);
- new->str = strdup(str);
- new->len = len;
- new->next = NULL;
- if (*head == NULL)
- {
- *head = new;
- return (new);
- }
- while (temp->next)
- temp = temp->next;
- temp->next = new;
- return (new);
+	listint_t *new, *current;
+
+	new = malloc(sizeof(listint_t));
+	if (new == NULL)
+		return (NULL);
+	new->n = n;
+	new->next = NULL;
+
+	if (*head == NULL)
+	{
+		*head = new;
+		return (*head);
+	}
+	current = *head;
+	while (current->next != NULL)
+		current = current->next;
+	current->next = new;
+	return (*head);
 }
--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -1,17 +1,15 @@
 #include <stdlib.h>
 #include "lists.h"
+#include "lists.h"
+#include <stdlib.h>
 /**
- * free_list - frees a linked list
- * @head: list_t list to be freed
- */
-void free_list(list_t *head)
+  * free_listint - define function
+  * @head: describe argument
+  */
+void free_listint(listint_t *head)
 {
- list_t *temp;
- while (head)
- {
- temp = head->next;
- free(head->str);
- free(head);
- head = temp;
- }
+	if (head == NULL)
+		return;
+	free_listint(head->next);
+	free(head);
 }
